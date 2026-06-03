21f557d5de77bc6c77425df1d010af9a0fc0511d
Srinidhi-tc <srinidhi.saas@gmail.com>
2026-06-03 11:36:33 -0700

    Update Home.jsx
    commenting community off

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Hero />
       <WorkSection />
       <PlayPreview />
-      <CommunityPreview />
+    {/*  <CommunityPreview /> */}
       <ContactCTA />
     </>
   );

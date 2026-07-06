13a272a29b328ea69907ff74f2afb76cfdeeb543
Srinidhi-tc <srinidhi.saas@gmail.com>
2026-07-06 14:25:20 -0700

    Update Beefeeder.jsx

--- src/pages/work/Beefeeder.jsx
+++ src/pages/work/Beefeeder.jsx
@@ -1,1 +1,1 @@
-export default function Malli() { return <div style={{padding:80}}>Coming soon.</div>; }
+export default function BeeFeeder() { return <div style={{padding:80}}>Coming soon.</div>; }
